cbd56a3e725e04c37027da58441a9ffb590b6467
Thomas <ringuede.thomas@gmail.com>
2022-06-10 16:35:49 +0200

    update Yify provider

--- src/Provider/Yify.php
+++ src/Provider/Yify.php
@@ -27,81 +27,32 @@ class Yify implements Provider
         $results = new ProviderResults();
         $url = sprintf($this->providerInformation->getSearchUrl()->getUrl(), $keywords->urlEncode(false));
         $crawler = $this->initDomCrawler($url);
-
-        $resultsFound = (int) $this->findText($crawler->filter('div.browse-content h2 b'));
-        if (1 !== $resultsFound) {
-
-            return [];
-        }
-
-        $pageLink = $this->findAttribute($crawler->filter('div.browse-content section div.row a.browse-movie-link'), 'href');
-        if (null === $pageLink) {
-
-            return [];
-        }
-        $detailPageCrawler = $this->initDomCrawler($pageLink);
-
-        foreach ($detailPageCrawler->filter('div.modal-content div.modal-torrent') as $item) {
+        foreach ($crawler->filter('div.browse-movie-wrap') as $item) {
             $itemCrawler = new Crawler($item);
-            $resolutionValue = $this->findText($itemCrawler->filter('div.modal-quality'));
-            if ($resolutionValue !== $keywords->getResolution()->getValue()) {
-
-                continue;
+            $title = $itemCrawler->filter('a.browse-movie-title')->text();
+            $year = $itemCrawler->filter('div.browse-movie-year')->text();
+            $fullTitle = "$title $year";
+            $pageLink = $itemCrawler->filter('a.browse-movie-title')->attr('href');
+            $detailPageCrawler = $this->initDomCrawler($pageLink);
+            foreach ($detailPageCrawler->filter('div.modal-torrent') as $modalTorrent) {
+                $modalTorrentCrawler = new Crawler($modalTorrent);
+                $resolution = Resolution::guessFromString($modalTorrentCrawler->filter('div.modal-quality')->text());
+                $fullTitleResolution = sprintf('%s %s', $fullTitle, $resolution->getValue());
+                try {
+                    $size = Size::fromHumanSize($modalTorrentCrawler->filter('p.quality-size')->eq(1)->text());
+                    $magnet = $modalTorrentCrawler->filter('a.magnet-download')->attr('href');
+                } catch (\Exception $exception) {
+                    continue;
+                }
+                $results->add(new ProviderResult(
+                    $this->getName(),
+                    new TorrentData($fullTitleResolution, $magnet, 100, $resolution),
+                    $size
+                ));
             }
-
-            $resolution = Resolution::guessFromString($resolutionValue);
-
-            $type = $this->findText($itemCrawler->filter('p.quality-size')->eq(0));
-            $humanSize = $this->findText($itemCrawler->filter('p.quality-size')->eq(1));
-
-            if (null === $humanSize) {
-                continue;
-            }
-
-            $size = Size::fromHumanSize($humanSize);
-
-            $magnet = $this->findAttribute($itemCrawler->filter('a.magnet-download'), 'href');
-
-            $seeds = $this->findSeeds($detailPageCrawler->filter('div#movie-tech-specs'), $resolution, $type);
-
-            $metaData = new TorrentData(
-                sprintf('%s %s', $keywords->getQuery(), $resolution->getValue()),
-                $magnet,
-                $seeds,
-                $resolution
-            );
-
-            $results->add(new ProviderResult($this->providerInformation->getName(), $metaData, $size));
         }
 
         return $results->getResults();
-    }
-
-    private function findSeeds(Crawler $crawler, Resolution $resolution, string $type): int
-    {
-        $specPosition = 0;
-        $index = 0;
-        $typeMapping = [
-            'BluRay' => 'BLU',
-            'WEB' => 'WEB',
-        ];
-        foreach ($crawler->filter('span.tech-quality') as $span) {
-            $crawlerSpan = new Crawler($span);
-            if (preg_match(sprintf('/.*%s\.%s.*/i', $resolution->getValue(), $typeMapping[$type]), $crawlerSpan->text())) {
-                $specPosition = $index;
-                break;
-            }
-            $index++;
-        }
-
-
-        $peersAndSeedsString = $this->findText($crawler->filter('div.tech-spec-info span.tech-peers-seeds')->eq($specPosition)->parents());
-        if (!preg_match('/\d+\s\/\s(\d+,?\d+)/i', $peersAndSeedsString, $match)) {
-
-            return 0;
-        }
-
-        return (int) str_replace(',', '', $match[1]);
     }
 
     public function getName(): string
